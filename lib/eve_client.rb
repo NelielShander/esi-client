e715eb9ed7f0f5002fc80219c7d687b04277e5c3
Mikhail <mrubtsov@onlinetours.ru>
2023-08-16 14:08:46 +0700

    sample client

--- lib/eve_client.rb
+++ lib/eve_client.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
+require 'net/http'
+require 'json'
+
 require_relative 'eve_client/version'
 require_relative 'eve_client/esi_auth_native'
-require_relative 'eve_client/esi_auth_web'
 require_relative 'eve_client/configuration'
 require_relative 'eve_client/scope'
 
@@ -11,6 +13,22 @@ module EveClient
   module_function
 
   def sample_request
-    Scope::Alliances.new.read_contacts
+    esi_auth = EsiAuthNative.new
+    scope = 'esi-assets.read_assets.v1'
+    esi_auth.authorize scope
+    token = esi_auth.jwt_token['access_token']
+    character_id = esi_auth.decode_payload.dig(0, 'sub').split(':').last
+
+    get_request "characters/#{character_id}/assets/", token
+  end
+
+  def get_request(path, token = nil)
+    uri = URI.join('https://esi.evetech.net/latest/', path)
+    uri.query = URI.encode_www_form({ datasource: 'tranquility' })
+    headers = { 'accept': 'application/json', 'cache-control': 'no-cache' }
+    headers.merge! 'authorization': "Bearer #{token}" if token
+
+    response = Net::HTTP.get_response(uri, **headers)
+    JSON.parse response.body
   end
 end
